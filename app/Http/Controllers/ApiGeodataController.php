300f0fa5d42cec197f7a431f37efe3b1a4eb97ac
Bartek Witkowski <bartek@tibe.no>
2023-06-29 23:24:30 +0200

    refactor: ApiGeoDataController

--- app/Http/Controllers/ApiGeodataController.php
+++ app/Http/Controllers/ApiGeodataController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Services\BringApiService;
+use App\Services\Parser;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 
@@ -16,7 +17,9 @@ class ApiGeodataController extends Controller
     public function index(Request $request)
     {
         $geodataService = new BringApiService();
-        return $geodataService->get('postal-codes');
+        $code = $request->query('code');
+        $parsed = Parser::fromPostalCode($geodataService->get('postal-codes/' . $code));
+        return $parsed;
     }
 
 }
